219bbb89c309873a7bed32317471f3a302a3d773
Philipp Gabler <phipsgabler@users.noreply.github.com>
2017-12-03 10:48:08 +0100

    Draft for parametrized macro

--- src/utils.jl
+++ src/utils.jl
@@ -16,3 +16,21 @@ end
 (pf::ParametrizedFunction)(x) = pf.f(x, pf.parameters...)
 
 
+# macro parametrized(fundef)
+#     fsplit = splitdef(fundef)
+#     tname = gensym(fsplit[:name])
+#     obj = gensym(:fp)
+#     arg = gensym(:x)
+#     funfield = gensym(:fun)
+#     kwargs = [gensym(kw) for kw in fsplit[:kwargs]]
+
+#     _struct = Expr(:struct, true, # mutable = true
+#                    Expr(:(<:), tname, :Function),
+#                    Expr(:block, funfield, kwargs...))
+#     _call = Expr(:(=), Expr(:call, Expr(:(::), obj, tname), arg),
+#                  Expr(:call,
+#                       Expr(:(.), obj, Meta.quot(funfield)),
+#                       arg,
+#                       (Expr(:(.), obj, Meta.quot(kw)) for kw in kwargs)...))
+#     Expr(:block, _struct, _call)
+# end
