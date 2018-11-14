2a3ec28a1eb27c2c3f351277ae50904429150fc4
Philipp Gabler <phipsgabler@users.noreply.github.com>
2018-11-14 10:17:54 +0100

    Make crossover default copying

--- src/crossover.jl
+++ src/crossover.jl
@@ -10,9 +10,9 @@ setup!(strategy::CrossoverStrategy, model) = strategy
     crossover!(children, parents, selection, strategy[, generation]) -> children
 
 Perform crossover on `parents[selection]`, and write the result to `children`.  Return the new
-`children`.
+`children`.  The resulting children should be new or copied.
 """
-crossover!(children,  parents, selection, ::CrossoverStrategy) = children .= population
+crossover!(children,  parents, selection, ::CrossoverStrategy) = copyto!(children, population)
 crossover!(children,  parents, selection, strategy::CrossoverStrategy, generation) =
     crossover!(children, parents, selection, strategy)
 
