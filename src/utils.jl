9f71354c2590e600627d2b7d8f222f9fa4992fd9
Philipp Gabler <phipsgabler@users.noreply.github.com>
2018-11-14 20:42:29 +0100

    Add Shifted transformation for distributions

--- src/utils.jl
+++ src/utils.jl
@@ -1,3 +1,7 @@
+import Distributions
+const D = Distributions
+import Base: +
+
 export ParametrizedFunction
 
 struct TimeInfo
@@ -15,6 +19,8 @@ macro timeinfo(expr)
         val, TimeInfo(time, bytes, gctime, memallocs)
     end
 end
+
+maximumby(f, collection) = collection[argmax(f(x) for x in collection)]
 
 # see https://github.com/JuliaLang/julia/blob/master/doc/src/devdocs/ast.md
 # using MacroTools
@@ -36,3 +42,30 @@ end
 #                       (Expr(:(.), obj, Meta.quot(kw)) for kw in kwargs)...))
 #     Expr(:block, _struct, _call)
 # end
+
+
+# Will work if `D` is a location family.
+struct Shifted{T<:D.ContinuousUnivariateDistribution} <: D.ContinuousUnivariateDistribution
+    dist::T
+    δ::Float64
+end
+
+Shifted(dist::T, δ) where {T} = Shifted{T}(dist, convert(Float64, δ))
+
+D.rand(d::Shifted) = D.rand(d.dist) + d.δ
+D.pdf(d::Shifted, x::Real) = D.pdf(d.dist, x - d.δ)
+D.logpdf(d::Shifted, x::Real) = D.logpdf(d.dist, x - d.δ)
+D.cdf(d::Shifted, x::Real) = D.cdf(d.dist, x - d.δ)
+D.quantile(d::Shifted, q::Real) = D.quantile(d.dist, q) + d.δ
+D.minimum(d::Shifted) = D.minimum(d.dist) + d.δ
+D.maximum(d::Shifted) = D.maximum(d.dist) + d.δ
+D.insupport(d::Shifted, x::Real) = D.insupport(d.dist, x - d.δ)
+D.mean(d::Shifted) = D.mean(d.dist) + d.δ
+D.var(d::Shifted) = D.var(d.dist)
+D.modes(d::Shifted) = D.modes(d.dist) .+ d.δ
+D.mode(d::Shifted) = D.mode(d.dist) + d.δ
+# StatsBase.skewness(d::Shifted) = StatsBase.skewness(d.D)
+# StatsBase.kurtosis(d::Distribution, correction::Bool) = StatsBase.kurtosis(d.D, correction)
+# StatsBase.entropy(d::Shifted, b::Real) = StatsBase.entropy(d.D, b)
+# mgf(d::Shifted, ::Any) = ???
+# cf(d::Shifted, ::Any) = ???
